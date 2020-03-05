e514792fd0add403cfd3e7a94c74992ccf52c55c
Justin Ho <justinhodev@gmail.com>
2020-03-05 01:01:14 -0800

    Add Toggle to Form Buttons

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,6 @@
 'use strict';
-
+// Add toggling mobile menu icon
+// from https://developer.mozilla.org/en-US/docs/Web/API/Element/classList/
 var mobileMenu = document.querySelector('.nav__btn-group--mobile');
 var mobileMenuIcon = document.querySelector('.nav__mobile-icon');
 
@@ -13,3 +14,13 @@ function toggleMenu() {
   }
 }
 
+// Add accessible toggle buttons
+// from https://smashingmagazine.com/2017/09/building-inclusive-toggle-buttons/
+var toggleBtnList = document.querySelectorAll('[aria-pressed]');
+
+toggleBtnList.forEach(function (toggleButton) {
+  toggleButton.addEventListener('click', function(e) {
+    var pressed = e.target.getAttribute('aria-pressed') === 'true';
+    e.target.setAttribute('aria-pressed', String(!pressed));
+  });
+});
